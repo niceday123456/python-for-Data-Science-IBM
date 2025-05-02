475e8b49b24cde8b77327d4be13d5649dc5836d1
MuazTasnim <tasnimmuaz100@gmail.com>
2025-05-02 09:42:26 +0200

    nothing special also

---
+++ new_2.cpp
@@ -0,0 +1,5 @@
+//
+// Created by muazt on 02/05/2025.
+//
+
+#include "new_2.h"
---
+++ new_2.h
@@ -0,0 +1,16 @@
+//
+// Created by muazt on 02/05/2025.
+//
+
+#ifndef NEW_2_H
+#define NEW_2_H
+
+
+
+class new_2 {
+
+};
+
+
+
+#endif //NEW_2_H
